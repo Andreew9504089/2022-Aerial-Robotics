65e754f511eb2f3d0908287791c3bb2d8f071ab8
AndrewtheDreamer <61780723+Andreew9504089@users.noreply.github.com>
2022-04-08 11:00:04 +0800

    Update hw4_1.m

--- Homework/hw4/hw4_1.m
+++ Homework/hw4/hw4_1.m
@@ -3,26 +3,27 @@ clear all;
 
 data1 = readmatrix("HW4-1.xls");
 
-x1 = data1(:,1);
-y1 = data1(:,2);
-output1 = data1(:,3);
+A = data1(:,1:2);
+Y = data1(:,3);
 
-a = 0;
-b = 0;
-for i = 1:length(x1)
-    a = a + x1(i)*x1(i);
-    b = b + x1(i)*y1(i);
-end
+x = inv(A' * A) * A' * Y;
 
-p = b/a
+%a = 0;
+%b = 0;
+%for i = 1:length(x1)
+%    a = a + x1(i)*x1(i);
+%    b = b + x1(i)*y1(i);
+%end
 
-figure(1)
-x = 0:0.1:max(x1);
-y = p*x;
-plot(x,y, 'color', 'red')
-hold on
-plot(x1, y1, '.', 'markersize', 8, 'color', 'blue')
-hold off
+%p = b/a
+
+%figure(1)
+%x = 0:0.1:max(x1);
+%y = p*x;
+%plot(x,y, 'color', 'red')
+%hold on
+%plot(x1, y1, '.', 'markersize', 8, 'color', 'blue')
+%hold off
 
 
 
